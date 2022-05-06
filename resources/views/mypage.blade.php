1135fac96906011781d906f9a118333427e2e781
gulkei <piyokuzirapiyo@gmail.com>
2022-05-06 18:14:06 +0900

    マイページ作成

---
+++ resources/views/mypage.blade.php
@@ -0,0 +1,17 @@
+<x-public.base>
+  @section('title', '予約システム - マイページ')
+  @section('content')
+  <x-public.layouts.header />
+
+  <main class="main">
+    <div class="mypage">
+      <div class="container">
+        <p>マイページ</p>
+      </div>
+    </div>
+  </main>
+
+  <x-public.layouts.footer />
+
+  @endsection
+</x-public.base>
