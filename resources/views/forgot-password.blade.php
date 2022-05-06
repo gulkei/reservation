a2da3b019f45ac5dd9e493785057ad3e4ad72bb8
gulkei <piyokuzirapiyo@gmail.com>
2022-05-06 16:18:36 +0900

    パスワード再設定リンク送信処理作成

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -8,13 +8,20 @@
     <div class="send-password">
       <div class="container">
         <h3 class="heading-tertiary">パスワード再設定</h3>
-        <form action="" method="post" class="form">
+        @if (session('status'))
+        <p>{{session('status')}}</p>
+        @endif
+        @error('email')
+        <p class="form__error">{{$message}}</p>
+        @enderror
+        <form action="{{route('password.email')}}" method="post" class="form">
+          @csrf
           <div class="form__group">
             <label for="">メールアドレス</label>
-            <input type="email">
+            <input type="email" name="email" required>
           </div>
           <div class="button-wrap margin-top-helper">
-            <a href="#" class="button">送信する</a>
+            <input type="submit" class="button" value="送信する">
           </div>
         </form>
       </div>
--- routes/web/forgot-password.php
+++ routes/web/forgot-password.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ForgotPasswordController;
 
 Route::get('/forgot-password', [ForgotPasswordController::class, 'index'])->name('password.request');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'send'])->name('password.email');
