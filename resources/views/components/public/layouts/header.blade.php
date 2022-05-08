63ff421cb07ebd03ecb3887c46cc07c741926be4
gulkei <piyokuzirapiyo@gmail.com>
2022-05-08 10:01:14 +0900

    ログイン時の項目変更

--- resources/views/components/public/layouts/header.blade.php
+++ resources/views/components/public/layouts/header.blade.php
@@ -3,7 +3,7 @@
     <h1 class="header__heading">
       <a href="{{route('home')}}"><img src="{{asset('img/logo.jpg')}}" alt="ロゴ"></a>
     </h1>
-    <nav class="nav">
+    {{-- <nav class="nav">
       <ul class="nav__list">
         <li class="nav__item">
           <a href="" class="nav__link">メニュー一覧</a>
@@ -15,8 +15,20 @@
           <a href="" class="nav__link">コース一覧</a>
         </li>
       </ul>
-    </nav>
+    </nav> --}}
 
+    @auth
+    <div class="main-buttons">
+      <div class="button-wrap--small">
+        <a href="" class="button">ログアウト</a>
+      </div>
+      <div class="button-wrap--small">
+        <a href="{{route('mypage')}}" class="button">マイページ</a>
+      </div>
+    </div>
+    @endauth
+
+    @guest
     <div class="main-buttons">
       <div class="button-wrap--small">
         <a href="{{route('mypage.login')}}" class="button">ログイン</a>
@@ -25,5 +37,6 @@
         <a href="{{route('register')}}" class="button">会員登録</a>
       </div>
     </div>
+    @endguest
   </div>
 </header>
