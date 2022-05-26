60c240b9b597c7b953db15bc770a941f23baffaa
gulkei <piyokuzirapiyo@gmail.com>
2022-05-26 15:19:40 +0900

    Profileサービスクラスの作成

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Http\Requests\User\UpdateRequest;
+use App\Services\ProfileService;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +13,6 @@ class ProfileController extends Controller
 
   public function index()
   {
-
     $user = Auth::user();
 
     return view('profile', [
@@ -24,35 +23,10 @@ class ProfileController extends Controller
   /**
    * ユーザ情報更新
    * @param  \App\Http\Requests\User\UpdateRequest  $request
-   * @param  \App\Models\User  $user
    * @return \Illuminate\Http\Response
    */
-  public function update(UpdateRequest $request)
+  public function update(ProfileService $profileService, UpdateRequest $request)
   {
-    $user = Auth::user();
-
-    $user->name = $request['name'];
-    $user->email = $request['email'];
-
-    // NOTE: passwordは任意のため、nullの場合とそうでない場合がある。
-    // nullableでnull許容しているが、
-    // null,空文字の場合は値をセットしたくないため、
-    // emptyで判定している。
-    if (!empty($request['password'])) {
-      $user->password = Hash::make($request['password']);
-    }
-
-    try {
-      $user->save();
-    } catch (\Exception $e) {
-      Log::error($e);
-      Log::error('プロフィールの更新に失敗しました。');
-
-      return back()->withErrors([
-        'all' => 'エラーが発生しました。もう一度やり直してください',
-      ])->onlyInput('name', 'email');
-    }
-
-    return redirect()->intended('mypage');
+    return $profileService->update($request);
   }
 }
