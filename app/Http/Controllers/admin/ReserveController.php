aa4de5ebc11dfc1fae3d9b71fe6b5637ed3b332c
gulkei <piyokuzirapiyo@gmail.com>
2022-06-03 17:02:53 +0900

    管理画面 ReserveServiceクラス作成

--- app/Http/Controllers/admin/ReserveController.php
+++ app/Http/Controllers/admin/ReserveController.php
@@ -2,94 +2,23 @@
 
 namespace App\Http\Controllers\admin;
 
-use Carbon\Carbon;
+use App\Services\admin\ReserveService;
 
 class ReserveController extends Controller
 {
 
-  public function index()
+  public function index(ReserveService $reserveService)
   {
-    // カレンダー
-    // 7日が6回の配列で取得
-    // 空のところは、曜日と日付から差で判別して入れ込む
-    $dayOfWeeks = ['日', '月', '火', '水', '木', '金', '土'];
+    $calendar = $reserveService->createCalendar();
 
-    $carbon = new Carbon();
+    $yearMonth = $reserveService->getYearMonth();
+    $today = $reserveService->getDayOfToday();
 
-    // dd($carbon->today()->dayOfWeek);
-
-    // $carbon->addDay(3);
-    $today = $carbon->today()->format('j日');
-
-    // 月の初めの曜日取得
-    $dayOfWeekNum = $carbon->startOfMonth()->dayOfWeek;
-
-    $calendar = collect();
-    // loopで使用
-    $weeks = collect();
-    // weeksの最終日代入
-    $lastDay = 0;
-    // loop回数(月の初めから最後までカバーできる 週7日 * 5 = 35)
-    $loopNum = 5;
-
-    $format = 'j日';
-    $formatMonthDay = 'n月j日';
-
-    for ($j = 0; $j < $loopNum; $j++) {
-
-      // 初回のみ
-      if (count($calendar) == 0) {
-
-        /**
-         * [
-         * 0 => '29',
-         * 1 => '30',
-         * 2 => '31',
-         * 3 => '1',
-         * 4 => '2',
-         * 5 => '3',
-         * 6 => '4',
-         * ]
-         *
-         * 上の配列の形式を作成
-         * 月初の曜日を基準、そこからマイナスしたりプラスしたり
-         */
-        for ($i = 0; $i < count($dayOfWeeks); $i++) {
-
-          // マイナス
-          if ($i < $dayOfWeekNum) {
-            $subDayNum = $dayOfWeekNum - $i;
-            $weeks->push($carbon->copy()->subDays($subDayNum)->format($format));
-          } elseif ($i == $dayOfWeekNum) {
-            // 曜日が同じ == 月初
-            $weeks->push($carbon->copy()->format($formatMonthDay));
-          } else {
-            // プラス
-            $addDayNum = $i - $dayOfWeekNum;
-            $weeks->push($carbon->copy()->addDays($addDayNum)->format($format));
-          }
-        }
-      } else {
-
-        for ($i = 0; $i < count($dayOfWeeks); $i++) {
-          $addDayNum = $lastDay + $i;
-          $weeks->push($carbon->copy()->addDays($addDayNum)->format($format));
-        }
-      }
-
-      $calendar->push($weeks);
-
-      $date = rtrim((string)$weeks[6], '日');
-
-      $lastDay = $date;
-
-      $weeks = collect();
-    }
-
-    // dd($calendar);
+    // 予約取得
 
     return view('admin.reserve', [
       'calendar' => $calendar,
+      'yearMonth' => $yearMonth,
       'today' => $today,
     ]);
   }
--- resources/views/admin/reserve.blade.php
+++ resources/views/admin/reserve.blade.php
@@ -9,56 +9,58 @@
 
       <main class="col-md-9 ms-auto col-lg-10 px-md-4 py-4">
         <h1 class="h2">予約</h1>
-        <p>予約</p>
+        <p>予約状況の確認</p>
 
         {{-- カレンダーを作成する --}}
         {{-- 月日のノーマルカレンダー --}}
         {{-- 日付に予約何件という形で表示 --}}
         {{-- その日付をクリックすれば、予約詳細が確認できる --}}
 
-        <table class="calendar">
-          <thead>
-            <tr>
-              <th>日</th>
-              <th>月</th>
-              <th>火</th>
-              <th>水</th>
-              <th>木</th>
-              <th>金</th>
-              <th>土</th>
-            </tr>
-          </thead>
+        <div class="calendar">
+          <p class="calendar__month">{{ $yearMonth }}</p>
 
-          <tbody>
-            @foreach ($calendar as $weeks)
+          <table>
+            <thead>
               <tr>
-                @foreach ($weeks as $day)
-
-                  <td>
-                    @if ($today == $day)
-                    <span class="calendar__day active">{{ $day }}</span>
-                    @php
-                      $today = false;
-                    @endphp
-                    @else
-                    <span class="calendar__day">{{ $day }}</span>
-                    @endif
-                  </td>
-                @endforeach
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th>
               </tr>
-            @endforeach
-            {{-- <td>
-              <span class="calendar__day">{{ $week[6] }}</span>
-              <div class="calendar__user">
-                <span>田中太郎</span>
-                <span>11:00</span>
-              </div>
-            </td> --}}
+            </thead>
 
-          </tbody>
-        </table>
+            <tbody>
+              @foreach ($calendar as $weeks)
+                <tr>
+                  @foreach ($weeks as $day)
 
+                    <td>
+                      @if ($today == $day)
+                      <span class="calendar__day active">{{ $day }}</span>
+                      @php
+                        $today = false;
+                      @endphp
+                      @else
+                      <span class="calendar__day">{{ $day }}</span>
+                      @endif
+                    </td>
+                  @endforeach
+                </tr>
+              @endforeach
+              {{-- <td>
+                <span class="calendar__day">{{ $week[6] }}</span>
+                <div class="calendar__user">
+                  <span>田中太郎</span>
+                  <span>11:00</span>
+                </div>
+              </td> --}}
 
+            </tbody>
+          </table>
+        </div>
 
 
         <footer class="pt-5 d-flex justify-content-between">
